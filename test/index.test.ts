086bb6cd3532c8dfb657a4898a30e00bec0e65da
Anders Fischer-Nielsen <andersfischern@me.com>
2022-08-31 10:24:41 +0200

    Clean up test scaffolding

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,22 +1,21 @@
-// You can import your modules
-// import index from '../src/index'
+import nock from 'nock';
+import { Probot, ProbotOctokit } from 'probot';
+import {
+  describe, beforeEach, test, expect, afterEach,
+} from '@jest/globals';
+import fs from 'fs';
+import path from 'path';
+import probotApp from '../src/app';
+import payload from './fixtures/issues.opened.json';
 
-import nock from "nock";
-// Requiring our app implementation
-import myProbotApp from "../src";
-import { Probot, ProbotOctokit } from "probot";
-// Requiring our fixtures
-import payload from "./fixtures/issues.opened.json";
-const issueCreatedBody = { body: "Thanks for opening this issue!" };
-const fs = require("fs");
-const path = require("path");
+const pushBody = { };
 
 const privateKey = fs.readFileSync(
-  path.join(__dirname, "fixtures/mock-cert.pem"),
-  "utf-8"
+  path.join(__dirname, 'fixtures/mock-cert.pem'),
+  'utf-8',
 );
 
-describe("My Probot app", () => {
+describe('My Probot app', () => {
   let probot: any;
 
   beforeEach(() => {
@@ -31,29 +30,27 @@ describe("My Probot app", () => {
       }),
     });
     // Load our app into probot
-    probot.load(myProbotApp);
+    probot.load(probotApp);
   });
 
-  test("creates a comment when an issue is opened", async () => {
-    const mock = nock("https://api.github.com")
+  test('creates a comment when an issue is opened', async () => {
+    const mock = nock('https://api.github.com')
       // Test that we correctly return a test token
-      .post("/app/installations/2/access_tokens")
+      .post('/app/installations/2/access_tokens')
       .reply(200, {
-        token: "test",
+        token: 'test',
         permissions: {
-          issues: "write",
+          push: 'read',
         },
       })
 
-      // Test that a comment is posted
-      .post("/repos/hiimbex/testing-things/issues/1/comments", (body: any) => {
-        expect(body).toMatchObject(issueCreatedBody);
+      .post('/repos/probot-test', (body: any) => {
+        expect(body).toMatchObject(pushBody);
         return true;
       })
       .reply(200);
 
-    // Receive a webhook event
-    await probot.receive({ name: "issues", payload });
+    await probot.receive({ name: 'push', payload });
 
     expect(mock.pendingMocks()).toStrictEqual([]);
   });
@@ -63,12 +60,3 @@ describe("My Probot app", () => {
     nock.enableNetConnect();
   });
 });
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
-
-// For more information about using TypeScript in your tests, Jest recommends:
-// https://github.com/kulshekhar/ts-jest
-
-// For more information about testing with Nock see:
-// https://github.com/nock/nock
