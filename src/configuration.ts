9281ab4380ea5b09aacb781a5b8b9a830e47e664
Anders Fischer-Nielsen <andersfischern@me.com>
2022-10-13 14:15:23 +0200

    Use Pino JSON logging (#37)

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,9 +1,12 @@
+import { Logger } from 'probot'
 import { parse } from 'yaml'
 import { RepositoryDetails, RepositoryConfiguration, OctokitInstance } from './types'
 
 export const determineConfigurationChanges =
-  (fileName: string, repository: RepositoryDetails, sha: string) => async (octokit: Pick<OctokitInstance, 'repos'>) => {
-    console.debug(`Saw changes to ${fileName}.`)
+  (fileName: string, repository: RepositoryDetails, sha: string) =>
+  (log: Logger) =>
+  async (octokit: Pick<OctokitInstance, 'repos'>) => {
+    log.debug(`Saw changes to ${fileName}.`)
     const { data: fileContents } = await octokit.repos.getContent({
       ...repository,
       path: fileName,
@@ -12,8 +15,8 @@ export const determineConfigurationChanges =
 
     const { content } = fileContents as { content: string }
     const decodedContent = Buffer.from(content, 'base64').toString()
-    console.debug(`'${fileName}' contains:`)
-    console.debug(decodedContent)
+    log.debug(`'${fileName}' contains:`)
+    log.debug(decodedContent)
 
     const parsed: RepositoryConfiguration = parse(decodedContent)
     return parsed
