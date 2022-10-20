20b1f84793facda7d3a0efc8de1b5af932bb850e
Marcos Arranz <marcos.arranz@pleo.io>
2022-10-20 14:59:39 +0100

    Move compilation out of function to only compile once

--- src/schema-validator.ts
+++ src/schema-validator.ts
@@ -5,23 +5,23 @@ import templateSchema from './template-schema.json'
 import { Logger } from 'probot'
 
 const ajv = new Ajv()
+const validate = ajv.compile<RepositoryConfiguration>(templateSchema)
 
 export const validateTemplateConfiguration =
-  (input: string) =>
-  (log: Logger): TemplateValidation => {
-    const validate = ajv.compile<RepositoryConfiguration>(templateSchema)
-    const parsed = parse(input)
-    const isValid = validate(parsed)
+    (input: string) =>
+        (log: Logger): TemplateValidation => {
+            const parsed = parse(input)
+            const isValid = validate(parsed)
 
-    const errors = validate.errors?.map(error => error?.message)?.filter(error => error) ?? []
+            const errors = validate.errors?.map(error => error?.message)?.filter(error => error) ?? []
 
-    if (!isValid) {
-      log.debug(`Saw validation errors:`)
-      log.debug(errors)
-    }
+            if (!isValid) {
+                log.debug(`Saw validation errors:`)
+                log.debug(errors)
+            }
 
-    return {
-      result: isValid,
-      errors,
-    }
-  }
+            return {
+                result: isValid,
+                errors,
+            }
+        }
