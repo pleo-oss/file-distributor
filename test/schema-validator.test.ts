94387e8db3d8d5d8f88e767b6a9c928474572c51
Anders Fischer-Nielsen <andersfischern@me.com>
2022-10-20 11:12:35 +0200

    Fix schema validation test syntax

--- test/schema-validator.test.ts
+++ test/schema-validator.test.ts
@@ -1,19 +1,18 @@
-import validator from "../src/schema-validator";
+import { validateTemplateConfiguration } from '../src/schema-validator'
 import schema from '../src/template-schema.json'
-import { JSONSchemaType } from "ajv";
-import { TemplateConfig } from "../src/types";
-
-
-
+import { JSONSchemaType } from 'ajv'
+import { TemplateConfig } from '../src/types'
+import { Logger } from 'probot'
 
 describe('Schema Tests', () => {
-    test('returns false for an invalid input', async () => {
-        const validation = validator(schema as JSONSchemaType<TemplateConfig>, "DFds")
-        expect(validation).toBeFalsy()
-    })
+  const log = { info: () => ({}), error: () => ({}), debug: () => ({}) } as unknown as Logger
+  test('returns false for an invalid input', async () => {
+    const validation = validateTemplateConfiguration(schema as JSONSchemaType<TemplateConfig>, 'DFds')(log)
+    expect(validation).toBeFalsy()
+  })
 
-    test('returns true for a valid schema', async () => {
-        const input = `
+  test('returns true for a valid schema', async () => {
+    const input = `
         # The template version to use (optional).
         version: v10.7.0
         
@@ -25,7 +24,7 @@ describe('Schema Tests', () => {
           - source: path/to/template/filename.yaml
             destination: path/to/template-destination/filename.yaml
         `
-        const validation = validator(schema as JSONSchemaType<TemplateConfig>, input)
-        expect(validation).toBeTruthy()
-    })
+    const validation = validateTemplateConfiguration(schema as JSONSchemaType<TemplateConfig>, input)(log)
+    expect(validation).toBeTruthy()
+  })
 })
