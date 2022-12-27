b3e1f42969ada86911746e7a8a1d464ff8255d50
Anders Fischer-Nielsen <andersfischern@me.com>
2022-12-27 13:27:49 +0100

    Simplify module, narrow scopes

--- src/checks.ts
+++ src/checks.ts
@@ -1,140 +1,123 @@
-import { OctokitInstance, CreateCheckInput, UpdateCheckInput, ValidationError } from './types'
+import { OctokitInstance, ValidationError, Check } from './types'
 import { Logger } from 'probot'
+import { RestEndpointMethodTypes } from '@octokit/plugin-rest-endpoint-methods'
+
+const checkName = 'Configuration validation' as const
+const checkCompleted = 'completed' as const
+const checkTitle = 'Schema validation' as const
 
 export const checks = (log: Logger, octokit: Pick<OctokitInstance, 'checks'>) => {
-  const createCheckRun = async (input: CreateCheckInput) => {
-    const { owner, repo, sha } = input
+  const asError = (
+    check: RestEndpointMethodTypes['checks']['update']['parameters'] & {
+      output: {
+        title: string
+        summary: string
+      }
+    },
+  ) => {
+    return {
+      ...check,
+      output: {
+        ...check.output,
+        summary:
+          'There was an unexpected error running the check. Please try again and if the error persists contact the stewards.',
+      },
+    }
+  }
 
-    log.debug('Creating queued check run on %s.', input.sha)
+  const withErrors = (
+    check: RestEndpointMethodTypes['checks']['update']['parameters'],
+    errors: ValidationError[],
+    configFilePath: string | undefined,
+  ): RestEndpointMethodTypes['checks']['update']['parameters'] => {
+    const withoutErrorLine = errors.filter(e => !e.line)
+    const withErrorLine = errors.filter(e => e.line)
+    const text = withoutErrorLine.length > 0 ? withoutErrorLine.map(e => `- \`${e.message}\``).join('\n') : undefined
+
+    return {
+      ...check,
+      name: checkName,
+      status: checkCompleted,
+      output: {
+        title: checkTitle,
+        summary: 'The following errors are present:',
+        text: text,
+        annotations: withErrorLine.map(err => ({
+          path: configFilePath,
+          start_line: err.line,
+          end_line: err.line,
+          annotation_level: 'failure',
+          message: err.message,
+        })),
+      },
+    }
+  }
+
+  const createCheck = async (input: Check) => {
+    const { owner, repo, sha } = input
+    const newCheck = {
+      owner,
+      repo,
+      name: checkName,
+      head_sha: sha,
+      status: 'queued',
+      output: {
+        title: checkTitle,
+        summary: 'Validation queued',
+      },
+    }
+
+    log.debug('Creating queued check run on %s.', sha)
     const {
       data: { id },
-    } = await octokit.checks.create(createNewCheckInput(owner, repo, sha))
-    log.debug('Queued check run %s with ID %d.', input.sha, id)
+    } = await octokit.checks.create(newCheck)
+    log.debug('Queued check run %s with ID %d.', sha, id)
 
     return id
   }
 
-  const resolveCheckRun = async (input: UpdateCheckInput, configFilePath?: string) => {
-    const { repo, owner, checkRunId, sha, conclusion } = input
+  const updateCheck = async (parameters: RestEndpointMethodTypes['checks']['update']['parameters']) => {
+    const { check_run_id } = parameters
+    log.debug('Updating check run %d.', check_run_id)
+    const {
+      data: { conclusion: result },
+    } = await octokit.checks.update(parameters)
+    log.debug('Updated check run %d with conclusion %s.', check_run_id, result)
+    return result
+  }
 
-    if (input.conclusion === 'failure') {
-      return updateCheck(createCheckErrorInput(owner, repo, checkRunId, sha, conclusion))
-    } else if (input.conclusion === 'action_required') {
-      return await updateCheck(
-        createRequiredActionInput(owner, repo, checkRunId, sha, conclusion, input.errors, configFilePath),
-      )
-    } else {
-      return await updateCheck(createCheckSuccessInput(owner, repo, checkRunId, sha, conclusion))
+  const resolveCheck = async (input: Check, configFilePath?: string) => {
+    const { repo, owner, checkRunId, sha, conclusion, errors } = input
+
+    const check = {
+      owner,
+      repo,
+      name: checkName,
+      check_run_id: checkRunId,
+      status: checkCompleted,
+      head_sha: sha,
+      conclusion,
+      output: {
+        title: checkTitle,
+        summary: conclusion,
+      },
     }
 
-    async function updateCheck(params: {
-      owner: string
-      repo: string
-      name: string
-      check_run_id: number
-      status: string
-      head_sha: string
-      conclusion: string
-      output: { title: string; summary: string }
-    }) {
-      log.debug('Updating check run %d.', checkRunId)
-      const {
-        data: { conclusion: result },
-      } = await octokit.checks.update(params)
-      log.debug('Updated check run %d with conclusion %s.', checkRunId, result)
-      return result
+    switch (conclusion) {
+      case 'failure': {
+        return updateCheck(asError(check))
+      }
+      case 'action_required': {
+        return updateCheck(withErrors(check, errors, configFilePath))
+      }
+      default: {
+        return updateCheck(check)
+      }
     }
   }
 
   return {
-    createCheckRun,
-    resolveCheckRun,
-  }
-}
-
-function createNewCheckInput(owner: string, repo: string, sha: string) {
-  return {
-    owner,
-    repo,
-    name: 'Configuration validation',
-    head_sha: sha,
-    status: 'queued',
-    output: {
-      title: 'Schema validation',
-      summary: 'Validation queued',
-    },
-  }
-}
-
-function createCheckSuccessInput(owner: string, repo: string, checkRunId: number, sha: string, conclusion: string) {
-  return {
-    owner,
-    repo,
-    name: 'Configuration validation',
-    check_run_id: checkRunId,
-    status: 'completed',
-    head_sha: sha,
-    conclusion,
-    output: {
-      title: 'Schema validation',
-      summary: conclusion,
-    },
-  }
-}
-
-function createRequiredActionInput(
-  owner: string,
-  repo: string,
-  checkRunId: number,
-  sha: string,
-  conclusion: string,
-  errors: ValidationError[],
-  configFilePath: string | undefined,
-) {
-  const errorsWithoutLine = errors.filter(e => !e.line)
-  const errorsWithLine = errors.filter(e => e.line)
-  const text =
-    errorsWithoutLine.length > 0
-      ? `The following errors don't have a line associated: 
-            ${errorsWithoutLine.map(e => `- \`${e.message}\``).join('\n')}`
-      : undefined
-
-  return {
-    owner,
-    repo,
-    name: 'Configuration validation',
-    check_run_id: checkRunId,
-    status: 'completed',
-    head_sha: sha,
-    conclusion,
-    output: {
-      title: 'Schema validation',
-      summary: 'There has been some errors during the validation',
-      text: text,
-      annotations: errorsWithLine.map(err => ({
-        path: configFilePath,
-        start_line: err.line,
-        end_line: err.line,
-        annotation_level: 'failure',
-        message: err.message,
-      })),
-    },
-  }
-}
-
-function createCheckErrorInput(owner: string, repo: string, checkRunId: number, sha: string, conclusion: string) {
-  return {
-    owner,
-    repo,
-    name: 'Configuration validation',
-    check_run_id: checkRunId,
-    status: 'completed',
-    head_sha: sha,
-    conclusion,
-    output: {
-      title: 'Schema validation',
-      summary:
-        'There was an unexpected error running the check. Please try again and if the error persists contact the stewards.',
-    },
+    createCheck,
+    resolveCheck,
   }
 }
