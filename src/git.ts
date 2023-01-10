83736bba8475197b1789249623622f0a5d05dab0
Anders Fischer-Nielsen <andersfischern@me.com>
2023-01-10 16:10:18 +0100

    Ensure label creation on PR creation (#100)

--- src/git.ts
+++ src/git.ts
@@ -111,9 +111,10 @@ export const git = (log: Logger, octokit: Pick<OctokitInstance, 'pulls' | 'repos
     const labels = process.env['LABELS_TO_ADD']
     if (!labels) return number
 
+    const { owner, repo } = repository
     const asList = labels.split(',')
     try {
-      const added = await octokit.issues.setLabels({ ...repository, issue_number: number, labels: asList })
+      const added = await octokit.issues.addLabels({ owner, repo, issue_number: number, labels: asList })
       log.debug("Set label(s) '%o' on #%d.", added, number)
     } catch (e) {
       log.error('Failed to set labels on #%d: %o', number, e)
